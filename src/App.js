69c843c7ae3ccca75748751a22177d2090d49b19
prashnata chakrabotyr <prashanta0234@gmail.com>
2022-03-01 18:31:29 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import reactCSS from "reactcss";
+import { useState } from "react";
+import { CirclePicker } from "react-color";
 
 function App() {
+  const [displayColorPicker, setdisplayColorPicker] = useState(false);
+  const [colors, setColor] = useState({
+    r: "241",
+    g: "112",
+    b: "19",
+    a: "1",
+  });
+
+  const handleClick = () => {
+    setdisplayColorPicker(true);
+  };
+
+  const handleClose = () => {
+    setdisplayColorPicker(false);
+  };
+
+  const handleChange = (color) => {
+    setColor(color.rgb);
+    console.log(color);
+  };
+  const styles = reactCSS({
+    default: {
+      color: {
+        width: "40px",
+        height: "40px",
+        lineHeight: "40px",
+        borderRadius: "50%",
+        background: ` rgba( ${colors.r}, ${colors.g}, ${colors.b}, ${colors.a} )`,
+      },
+      swatch: {
+        display: "inline-block",
+        cursor: "pointer",
+      },
+      popover: {
+        position: "absolute",
+        zIndex: "2",
+      },
+      cover: {
+        position: "fixed",
+        top: "0px",
+        right: "0px",
+        bottom: "0px",
+        left: "0px",
+      },
+    },
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div style={styles.swatch} onClick={handleClick}>
+        <div style={styles.color}></div>
+      </div>
+      {displayColorPicker ? (
+        <div style={styles.popover}>
+          <div style={styles.cover} onClick={handleClose} />
+          <CirclePicker onChange={handleChange} />
+        </div>
+      ) : null}
     </div>
   );
 }
